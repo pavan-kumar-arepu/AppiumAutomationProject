7c8e4ea9db907cb8969be64dccc4328fa898bcbc
AREPU PAVAN KUMAR <iosdeveloper.ipa@gmail.com>
2024-01-10 23:07:35 +0100

    Tested on Android Emulator  - Sequence of clicks and scroll and enter data on textfiels. Just Run ElementBasicActions java file.

--- src/main/java/CreateDriverSession.java
+++ src/main/java/CreateDriverSession.java
@@ -16,11 +16,26 @@ public class CreateDriverSession {
 
         DesiredCapabilities caps = new DesiredCapabilities();
         caps.setCapability("platformName", platform);
-        caps.setCapability("newCommandTimeout", 300);
+//        caps.setCapability("newCommandTimeout", 300);
         
-        AppiumDriver driver = null;
+//        AppiumDriver driver = null;
 
-        URL url = new URL("http://0.0.0.0:4723/wd/hub");
+        URL url = new URL("http://0.0.0.0:4723/");
+
+        caps.setCapability("platformName", platform);
+        caps.setCapability("deviceName", "Pixel_5_API_33");
+        caps.setCapability("automationName", "UiAutomator2");
+        caps.setCapability("udid", "emulator-5554");
+        String androidAppUrl = System.getProperty("user.dir") + File.separator + "src" + File.separator + "main"
+                + File.separator + "resources" + File.separator + "ApiDemos-debug.apk";
+
+//                caps.setCapability("appPackage", "io.appium.android.apis");
+//                caps.setCapability("appActivity", "io.appium.android.ApiDemos");
+
+        caps.setCapability("app", androidAppUrl);
+
+        return new AndroidDriver(url, caps);
+        /*
 
         switch (platform) {
             case "Android":
@@ -28,13 +43,13 @@ public class CreateDriverSession {
                 caps.setCapability("deviceName", "Pixel_5_API_33");
                 caps.setCapability("automationName", "UiAutomator2");
                 caps.setCapability("udid", "emulator-5554");
-//                String appUrl = System.getProperty("user.dir") + File.separator + "src" + File.separator + "main"
-//                        + File.separator + "resources" + File.separator + "ApiDemos-debug.apk";
+                String androidAppUrl = System.getProperty("user.dir") + File.separator + "src" + File.separator + "main"
+                        + File.separator + "resources" + File.separator + "ApiDemos-debug.apk";
 
-                caps.setCapability("appPackage", "io.appium.android.apis");
-                caps.setCapability("appActivity", "io.appium.android.ApiDemos");
+//                caps.setCapability("appPackage", "io.appium.android.apis");
+//                caps.setCapability("appActivity", "io.appium.android.ApiDemos");
 
-//                caps.setCapability("app", appUrl);
+                caps.setCapability("app", androidAppUrl);
 
                 driver = new AndroidDriver(url, caps);
 
@@ -49,5 +64,7 @@ public class CreateDriverSession {
                 driver =  new IOSDriver(url, caps);
         }
         return driver;
+
+         */
     }
 }
